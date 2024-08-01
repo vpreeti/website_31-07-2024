67e0ebd5b50028032810e69735034604f5fbd78a
vpreeti <preeti.01123@gmail.com>
2024-08-01 17:12:42 +0530

    some more changes

--- components/OtherUtilities.jsx
+++ components/OtherUtilities.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../components/css/otherUtilities.css';
 
 const OtherUtilities = () => {
-  const [icons, setIcons] = useState([
+  const icons = [
     {
       src: 'public/images/logoNBPDCL.png',
       alt: 'NBPDCL',
@@ -18,28 +18,29 @@ const OtherUtilities = () => {
       alt: 'BSPGCL',
       name: 'BSPGCL',
     },
-    
+    {
+        src: 'public/images/logoNBPDCL.png',
+        alt: 'NBPDCL',
+        name: 'NBPDCL',
+      },
+      {
+        src: '../public/images/logoSBPDCL.png',
+        alt: 'SBPDCL',
+        name: 'SBPDCL',
+      },
+      {
+        src: '../public/images/logoBSPGCL.png',
+        alt: 'BSPGCL',
+        name: 'BSPGCL',
+      }
     // Add more icons as needed
-  ]);
-  
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % icons.length);
-    }, 2000); // 2 seconds
-
-    return () => clearInterval(intervalId);
-  }, [icons.length]);
+  ];
 
   return (
     <div className="other-utilities-container">
-      <ul className="other-utilities-list" style={{ transform: `translateX(-${currentIndex * 120}px)` }}>
-        {icons.concat(icons).map((icon, index) => (
-          <li
-            key={index}
-            className={`other-utilities-item ${index % icons.length === currentIndex ? 'active' : ''}`}
-          >
+      <ul className="other-utilities-list">
+        {icons.map((icon, index) => (
+          <li key={index} className="other-utilities-item">
             <img src={icon.src} alt={icon.alt} />
             <p>{icon.name}</p>
           </li>
